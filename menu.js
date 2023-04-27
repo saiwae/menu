020d4e873761acb3768e3be7b6baef666c615f45
khloe <slowykhloe@gmail.com>
2023-04-27 09:14:05 -0700

    initial second 4/25 4/27

--- menu.js
+++ menu.js
@@ -3,7 +3,7 @@ const readline= require('readline-sync');
 let order = [];
 let total = [];
 
-//step 1
+//step 1 khloe
 console.log('sandwiches choices:\nchicken: $5.25, beef: $6.25, tofu: $5.75');
 order[0] = readline.question('sandwich of your choice: ');
 if (order[0] == 'beef' || order[0] == 'Beef'){
@@ -13,9 +13,8 @@ if (order[0] == 'beef' || order[0] == 'Beef'){
 } else if (order[0] == 'tofu' || order[0] == 'Tofu'){
     total[0] = 5.75;
 }
-console.log(order);
 
-//step 2
+//step 2 khloe
 drink = readline.question('would you like a drink? (yes/no): ');
 if (drink == "yes" || drink == "Yes"){
     console.log('drink size choices:\nsmall: $1.00, medium: $1.75, large: $2.25');
@@ -30,10 +29,8 @@ if (drink == "yes" || drink == "Yes"){
 } else {
     total[1] = 0;
 }
-console.log(order);
-console.log(`total: $${total[0]+total[1]}`);
 
-// step 3
+// step 3 andrew
 fries = readline.question('would you like fries? (yes/no): ');
 if (fries == "yes" || fries == "Yes"){
     console.log('fries size choices:\nsmall: $1.00, medium: $1.50, large: $2.00');
@@ -54,17 +51,22 @@ if (fries == "yes" || fries == "Yes"){
 } else {
     total[2] = 0;
 }
-console.log(order);
-console.log(`total: $${total[0]+total[1]+total[2]}`);
 
-// step 4
+// step 4 andrew
 sauces = readline.question('would you like a ketchup? (yes/no): ');
-if (sauces == "yes" || sauces == "Yes")
+if (sauces == "yes" || sauces == "Yes"){
     console.log('one ketchup packet costs $0.25');
-total[3] = Number(readline.question('how many would you like: '))*.25;
+    total[3] = Number(readline.question('how many would you like: '))*.25;
+} else {
+    total[3] = 0;
+}
+if (total[0] >= 1 && total[1] >= 1 && total[2] >= 1){
+    total[4] = -1;
+} else {
+    total[4] = 0;
+}
 console.log(order);
-console.log(`total: $${total[0]+total[1]+total[2]+total[3]}`);
 
-// next steps
-total[5] = Number((total[0]+total[1]+total[2]+total[3]+total[4])*.07)+(total[0]+total[1]+total[2]+total[3]+total[4]).toFixed(2);
-console.log(total[5]);
+// next steps khloe
+total[5] = (((total[0]+total[1]+total[2]+total[3]+total[4])*.07)+(total[0]+total[1]+total[2]+total[3]+total[4])).toFixed(2);
+console.log(`total: $${total[5]}`);
